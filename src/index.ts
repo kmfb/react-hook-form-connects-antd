35ddeb465bf80318d891e811f0af7cdc28fd5599
ztx <zhaotianxion@gmail.com>
2022-07-29 09:38:24 +0000

    feat: add export types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+export * from "./types";
 export { default as Form } from "./Form";
 export { default as FormItem, PureFormItem } from "./FormItem";
-
 export { default as PlainText } from "./PlainText";
 export { ControledInput } from "./ControlAntd/ControlAntdFormItem/index";
 export { default as ControlAntdForm } from "./ControlAntd/ControlAntdForm/index";

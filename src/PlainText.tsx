176015bcb20e4f09c94e4ee8c46e715024819451
Star_dusted <zhaotianxion@gmail.com>
2022-08-09 13:45:40 +0000

    fix(PlainText): add the getDisplayValue

--- src/PlainText.tsx
+++ src/PlainText.tsx
@@ -4,16 +4,17 @@ import { isString } from './utils';
 export interface IProps extends React.HTMLAttributes<HTMLSpanElement> {
   value?: string;
   hidePlaceholder?: boolean;
+  getDisplayValue?: (value: string | undefined) => string;
 }
 
 const PlainText = React.forwardRef<any, IProps>((props, ref) => {
-  const { value, hidePlaceholder = false, placeholder = '-', ...spanProps } = props;
+  const { value, hidePlaceholder = false, placeholder = '-', getDisplayValue = (value) => value || "", ...spanProps } = props;
 
-  const displayValue = isString(value) ? value : value ? JSON.stringify(value) : undefined;
+  const formatValue: string | undefined = isString(value) ? value : value ? JSON.stringify(value) : "";
 
   return (
     <span ref={ref} {...spanProps}>
-      {displayValue || (!hidePlaceholder && placeholder)}
+      {getDisplayValue(formatValue) || (!hidePlaceholder && placeholder)}
     </span>
   );
 });
